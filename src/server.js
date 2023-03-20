1674f72e36750605e25bf9a0dc58926bd0c704cd
nicklesscs <tgao2@wpi.edu>
2023-03-20 08:15:35 -0400

    modify the server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,38 @@
-var http = require('http');
-var fs = require('fs');
+//var http = require('http');
+//var fs = require('fs');
+//
+//const PORT=8080;
+//
+//fs.readFile('./gui.html', function (err, html) {
+//
+//    if (err) throw err;
+//
+//    http.createServer(function(request, response) {
+//        response.writeHeader(200, {"Content-Type": "text/html"});
+//        response.write(html);
+//        response.end();
+//    }).listen(PORT,'130.215.175.244');
+//});
+//
+//
 
-const PORT=8080; 
+
+const http = require('http');
+const fs = require('fs');
+
+const PORT = 8080;
+const IP_ADDRESS = '130.215.175.244';
 
 fs.readFile('./gui.html', function (err, html) {
+  if (err) throw err;
 
-    if (err) throw err;    
+  const server = http.createServer(function(request, response) {
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(html);
+    response.end();
+  });
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+  server.listen(PORT, IP_ADDRESS, function () {
+    console.log(`Server running at http://${IP_ADDRESS}:${PORT}/`);
+  });
 });
