57db667d0345ae96b2ec4dbc899686113e557385
nicklesscs <tgao2@wpi.edu>
2023-03-27 11:20:28 -0400

    modify the client.js

--- src/client.js
+++ src/client.js
@@ -1,24 +1,4 @@
 const WebSocket = require('ws');
-const ipc = require('node-ipc');
-
-// Set up IPC connection to pose controller
-ipc.config.id = 'client';
-ipc.config.retry = 1500;
-ipc.config.silent = true;
-
-let poseControlConnection = false;
-ipc.connectTo('poseControl', function () {
-    poseControlConnection = true;
-    ipc.of.poseControl.on('connect', function () {
-        console.log('Connected to pose controller');
-    });
-    ipc.of.poseControl.on('disconnect', function () {
-        poseControlConnection = false;
-        console.log('Disconnected from pose controller');
-    });
-});
-
-//const WebSocket = require('ws');
 
 const ws = new WebSocket('ws://130.215.175.244:8080');
 
@@ -34,16 +14,54 @@ ws.on('close', function close() {
   console.log('disconnected from server');
 });
 
+//const WebSocket = require('ws');
+//const ipc = require('node-ipc');
+//
+//// Set up IPC connection to pose controller
+//ipc.config.id = 'client';
+//ipc.config.retry = 1500;
+//ipc.config.silent = true;
+//
+//let poseControlConnection = false;
+//ipc.connectTo('poseControl', function () {
+//    poseControlConnection = true;
+//    ipc.of.poseControl.on('connect', function () {
+//        console.log('Connected to pose controller');
+//    });
+//    ipc.of.poseControl.on('disconnect', function () {
+//        poseControlConnection = false;
+//        console.log('Disconnected from pose controller');
+//    });
+//});
+//
+////const WebSocket = require('ws');
+//
+//const ws = new WebSocket('ws://130.215.175.244:8080');
+//
+//ws.on('open', function open() {
+//  console.log('connected to server');
+//});
+//
+//ws.on('message', function incoming(data) {
+//  console.log(data);
+//});
+//
+//ws.on('close', function close() {
+//  console.log('disconnected from server');
+//});
+//
+//
+//ws.on('message', function incoming(message) {
+//    // Parse incoming message as JSON
+//    let data = JSON.parse(message);
+//
+//    // Check if pose controller is connected before sending data to it
+//    if (poseControlConnection) {
+//        // Send data to pose controller using IPC
+//        ipc.of.poseControl.emit('bodyData', data);
+//    }
+//});
 
-ws.on('message', function incoming(message) {
-    // Parse incoming message as JSON
-    let data = JSON.parse(message);
 
-    // Check if pose controller is connected before sending data to it
-    if (poseControlConnection) {
-        // Send data to pose controller using IPC
-        ipc.of.poseControl.emit('bodyData', data);
-    }
-});
 
 
