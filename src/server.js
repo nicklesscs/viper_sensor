1c2d49063dfe8fdae2f767340290568d395f787e
nicklesscs <tgao2@wpi.edu>
2023-04-07 12:45:12 -0400

    update the ip address and the roslib link

--- src/server.js
+++ src/server.js
@@ -53,7 +53,7 @@ wss.on('connection', (ws) => {
 
 // Listen for connections on the server
 server.listen(8080, () => {
-  console.log('Server running at http://130.215.120.242:8080/');
+  console.log('Server running at http://130.215.213.241:8080/');
 });
 
 function sendTfPolhemusDataToClient(data) {
